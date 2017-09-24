f4a441af9beafd523800ba10d0ff6db0e3742fdf
Surya <surya.yerramsetti@thomsonreuters.com>
2017-09-24 12:34:48 -0500

    package-lock.json and webpack.test.js

--- config/webpack.test.js
+++ config/webpack.test.js
@@ -2,52 +2,65 @@ var webpack = require('webpack');
 var helpers = require('./helpers');
 
 module.exports = {
-  devtool: 'inline-source-map',
+    devtool: 'inline-source-map',
 
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
 
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loaders: [
-          {
-            loader: 'awesome-typescript-loader',
-            options: { configFileName: helpers.root('src', 'tsconfig.json') }
-          } , 'angular2-template-loader'
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: {configFileName: helpers.root('src', 'tsconfig.json')}
+                    }, 'angular2-template-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'null-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'),
+                loader: 'null-loader'
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('src', 'app'),
+                loader: 'raw-loader'
+            },
+            /**
+             * Instruments JS files with Istanbul for subsequent code coverage reporting.
+             * Instrument only testing sources.
+             *
+             * See: https://github.com/deepsweet/istanbul-instrumenter-loader
+             */
+            {
+                enforce: 'post',
+                test: /\.ts$/,
+                loader: 'istanbul-instrumenter-loader',
+                include: helpers.root('src', 'app'),
+                exclude: /(node_modules|app\\spec)/,
+            }
         ]
-      },
-      {
-        test: /\.html$/,
-        loader: 'html-loader'
+    },
 
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-        loader: 'null-loader'
-      },
-      {
-        test: /\.css$/,
-        exclude: helpers.root('src', 'app'),
-        loader: 'null-loader'
-      },
-      {
-        test: /\.css$/,
-        include: helpers.root('src', 'app'),
-        loader: 'raw-loader'
-      }
+    plugins: [
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)@angular/,
+            helpers.root('./src'), // location of your src
+            {} // a map of your routes
+        )
     ]
-  },
-
-  plugins: [
-    new webpack.ContextReplacementPlugin(
-      // The (\\|\/) piece accounts for path separators in *nix and Windows
-      /angular(\\|\/)core(\\|\/)@angular/,
-      helpers.root('./src'), // location of your src
-      {} // a map of your routes
-    )
-  ]
 }
 
